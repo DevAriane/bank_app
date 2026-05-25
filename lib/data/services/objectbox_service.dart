386961ba0fc8262c4d9645500eddf940fa20c210
DevAriane <arianeateumo@gmail.com>
2026-05-25 08:59:36 +0100

    add objectbox

--- lib/data/services/objectbox_service.dart
+++ lib/data/services/objectbox_service.dart
@@ -1,1 +1,8 @@
 import 'package:get_x/get.dart';
+import '../../objectbox.g.dart';
+
+class ObjectboxService extends GetxController {
+  static ObjectboxService get to => Get.find<ObjectboxService>();
+
+  late final Store store;
+}
